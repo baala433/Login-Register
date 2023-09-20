304fdf859f9d7233b1c6eda6d3d92062974df065
BAALA B <115868506+baala433@users.noreply.github.com>
2023-09-20 14:41:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ signupLoginLink.forEach(link => {
         formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
     });
 });
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAc5g4wI1SHcF7lh14xU7QPPsUcZynES_k",
+    authDomain: "balabits2.firebaseapp.com",
+    databaseURL: "https://balabits2-default-rtdb.firebaseio.com",
+    projectId: "balabits2",
+    storageBucket: "balabits2.appspot.com",
+    messagingSenderId: "964857181963",
+    appId: "1:964857181963:web:0e1cdaf6d27edd2c2d540e",
+    measurementId: "G-0PP86WR9B7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
